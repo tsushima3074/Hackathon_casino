234a68dd7f2dc7b41c11cd8a07de8bb89942f813
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-01-31 15:05:31 +0900

    create,editファイル作成

--- account_edit.php
+++ account_edit.php
@@ -49,9 +49,92 @@
 
   var_dump($error_flag);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>アカウント編集</title>
+  <style>
+    .flex {
+      display: flex;
+    }
+    .justify-center {
+      justify-content: center;
+    }
+    .align-center {
+      align-items: center;
+    }
+    .title {
+      font-size: 40px;
+      font-weight: 600;
+    }
+    .subtitle {
+      font-size: 50px;
+      font-weight: 600;
+      text-align: center;
+      margin-bottom: 20px;
+      margin-top: 20px;
+    }
+    .box {
+      width: 40%;
+      height: 750px;
+      background-color: #E3E3E3;
+      border-radius: 50px;
+      border: 1px solid #000;
+      margin: 0 auto;
+      margin-top: 10px;
+    }
+    .account-form {
+      margin: 0 auto;
+    }
+    .f-p {
+      font-size: 30px;
+      margin-bottom: 0;
+      margin-top: 5px;
+    }
+    .form-div {
+      display: flex;
+    }
+    .input-center {
+      margin: 0 auto;
+      margin-left: 135px;
+    }
+    .f-input {
+      width: 70%;
+      font-size: 35px;
+    }
+    .f-btn {
+      width: 65%;
+      height: 50px;
+      font-size: 30px;
+      margin: 20px 0px 20px 135px;
+      color: #fff;
+      border: none;
+      background-color: #31C4FC;
+      border-radius: 50px;
+    }
+    .f-btn:hover {
+      cursor: pointer;
+    }
+    .move-login {
+      font-size: 25px;
+      margin-left: 130px;
+    }
+  </style>
+</head>
+<body>
+  <div>
+    <form action="account_edit.php" method="post">
+      <div class="input-center"><p class="f-p">メールアドレス</p><span class="flex"><img src="img/mail.png" width="40"><input type="text" name="mail" id="" class="f-input"></span></div>
+      <div class="input-center"><p class="f-p">ユーザ名</p><span class="flex"><img src="img/user.png" width="40"><input type="text" name="name" id="" class="f-input"></span></div><br>
+      <!-- メールアドレス: <input type="text" name="mail" id=""><br>
+      ユーザ名: <input type="text" name="name" id=""><br> -->
+      <button type="submit">送信</button>
+    </form>
+  </div>
+</body>
+</html>
 
-<form action="account_edit.php" method="post">
-  name: <input type="text" name="name" id=""><br>
-  mail: <input type="text" name="mail" id=""><br>
-  <button type="submit">送信</button>
-</form>
+
