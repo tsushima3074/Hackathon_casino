e659bc3ad5472b08dfb1391e26095a7774a15835
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-01-31 14:46:57 +0900

    account.php作成

--- create_account.php
+++ create_account.php
@@ -86,9 +86,11 @@
       font-weight: 600;
     }
     .subtitle {
-      font-size: 40px;
+      font-size: 50px;
       font-weight: 600;
       text-align: center;
+      margin-bottom: 20px;
+      margin-top: 20px;
     }
     .box {
       width: 40%;
@@ -105,20 +107,35 @@
     .f-p {
       font-size: 30px;
       margin-bottom: 0;
+      margin-top: 5px;
     }
     .form-div {
       display: flex;
     }
     .input-center {
       margin: 0 auto;
-      margin-left:  100px;
+      margin-left: 135px;
     }
     .f-input {
       width: 70%;
+      font-size: 35px;
     }
     .f-btn {
-      margin: 0 auto;
-
+      width: 65%;
+      height: 50px;
+      font-size: 30px;
+      margin: 20px 0px 20px 135px;
+      color: #fff;
+      border: none;
+      background-color: #31C4FC;
+      border-radius: 50px;
+    }
+    .f-btn:hover {
+      cursor: pointer;
+    }
+    .move-login {
+      font-size: 25px;
+      margin-left: 130px;
     }
   </style>
 </head>
@@ -134,9 +151,9 @@
       <div class="input-center"><p class="f-p">ユーザ名</p><span class="flex"><img src="img/user.png" width="40"><input type="text" name="name" id="" class="f-input"></span></div><br>
       <div class="input-center"><p class="f-p">パスワード</p><span class="flex"><img src="img/pass.png" width="40"><input type="text" name="pw" id="" class="f-input"></span></div><br>
       <div class="input-center"><p class="f-p">確認用パスワード</p><span class="flex"><img src="img/pass.png" width="40"><input type="text" name="re_pw" id="" class="f-input"></span></div><br>
-      <button type="submit" class="f-btn">送信</button>
+      <button type="submit" class="f-btn">アカウント作成</button>
     </form>
-    <a href="login.php">ログイン画面へ</a>
+    <a href="login.php" class="move-login">ログイン画面へ</a>
   </div>
 </body>
 </html>
