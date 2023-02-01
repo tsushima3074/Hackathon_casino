d17228851fe5d4347ae80f5799ff63be815e50cf
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-02-01 23:10:57 +0900

    景品交換ページ作成

---
+++ gift_trade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>景品交換所</title>
+  <link rel="stylesheet" href="src/css/gift_trade.css">
+  <?php include "header.php" ?>
+</head>
+<body>
+  <p class="subtitle">景品交換所</p>
+  <div class="column column03">
+    <ul>
+      <li><img src="img/gift1.jpg" /><p>商品名</p><span>交換ポイント : 2,600円</span><button class="trade-btn">交換する</button></li>
+      <li><img src="img/gift2.jpg" /><p>商品名</p><span>交換ポイント : 2,600円</span><button class="trade-btn">交換する</button></li>
+      <li><img src="img/gift3.jpg" /><p>商品名</p><span>交換ポイント : 2,600円</span><button class="trade-btn">交換する</button></li>
+      <li><img src="img/gift1.jpg" /><p>商品名</p><span>交換ポイント : 2,600円</span><button class="trade-btn">交換する</button></li>
+      <li><img src="img/gift2.jpg" /><p>商品名</p><span>交換ポイント : 2,600円</span><button class="trade-btn">交換する</button></li>
+      <li><img src="img/gift3.jpg" /><p>商品名</p><span>交換ポイント : 2,600円</span><button class="trade-btn">交換する</button></li>
+    </ul>
+  </div>
+</body>
+</html>
