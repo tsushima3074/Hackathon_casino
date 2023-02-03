2bfc9a67b06d91bdc14af0e4934c348eeb1e4231
tsushima3074 <y.tsushima.sys20@morijyobi.ac.jp>
2023-02-03 09:38:15 +0900

    バグの修正

--- slot_bet.php
+++ slot_bet.php
@@ -6,7 +6,7 @@
   if(isset($_SESSION["user"], $_POST["best"], $_POST["id"])) {
     $point_db = new point_db();
     $point = $point->get_user_point($_SESSION["user"]["id"]);
-    if($_POST["best"] > $point) {
+    if($_POST["best"] < $point) {
       point_function($_SESSION["user"]["id"], $_POST["id"], 1, $_POST["bet"]);
       $user_point = $point_db->update_point($_SESSION["user"]["id"], $_POST["bet"]);
     }
